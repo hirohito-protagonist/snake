d34c4225a2d9ddd6ec4a2b0e82363c3259038c4a
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2019-03-08 20:06:30 +0100

    chore: piston engine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate piston_window;
+use piston_window::*;
+
 fn main() {
-    println!("Snake");
+    let mut window: PistonWindow = WindowSettings::new("Snake", (640, 480))
+        .exit_on_esc(true)
+        .build()
+        .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |_c, g| {
+            clear([0.0, 0.0, 0.0, 1.0], g);
+        });
+    }
 }
