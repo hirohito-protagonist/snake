fa7cd1ca7cf90da387e154974926bbf10016f87d
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2019-03-10 17:51:29 +0100

    chore: draw util

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 extern crate piston_window;
 extern crate find_folder;
+
 use piston_window::*;
 
+mod draw;
+
+// use draw::draw_block;
+
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("Snake", (640, 480))
+    let width = 640;
+    let height = 480;
+    let mut window: PistonWindow = WindowSettings::new("Snake", (width,  height))
         .exit_on_esc(true)
         .build()
         .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
@@ -17,6 +24,10 @@ fn main() {
     window.set_lazy(true);
     while let Some(e) = window.next() {
         window.draw_2d(&e, |_c, g| {
+            
+            draw::draw_block([0.0, 1.0, 0.0, 1.0], 1, 2, &_c, g);
+            draw::draw_rectangle([0.0, 1.0, 0.0, 1.0], 0, 0, width as i32, 1, &_c, g);
+
             let transform = _c.transform.trans((640.0 / 2.0) - 70.0, 480.0 / 2.0);
 
             clear([0.0, 0.0, 0.0, 1.0], g);
