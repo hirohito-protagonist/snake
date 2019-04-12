a243a91bdad64a457f39e0c6e7d299a64a8771cd
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2019-04-12 16:44:12 +0200

    refactor: define food module

---
+++ src/food.rs
@@ -0,0 +1,33 @@
+use piston_window::{Context, G2d};
+use crate::draw::{draw_block};
+use crate::theme;
+
+pub struct Food {
+    x: i32,
+    y: i32,
+}
+
+impl Food {
+    pub fn new(x: i32, y: i32) -> Food {
+        Food{
+            x,
+            y
+        }
+    }
+
+    pub fn draw(&self, context: &Context, g: &mut G2d) {
+        draw_block(theme::FOOD_COLOR, self.x, self.y, context, g);
+    }
+
+    pub fn reposition(&mut self, width: i32, height: i32) {
+        let mut rng = thread_rng();
+        let new_x = rng.gen_range(1, width - 1);
+        let new_y = rng.gen_range(1, height - 1);
+        self.x = new_x;
+        self.y = new_y;
+    }
+
+    pub fn position(&self) -> (i32, i32) {
+        (self.x, self.y)
+    }
+}
