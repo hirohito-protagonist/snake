e531819cfbf51f0c687179dc03b7453b1871890b
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2019-05-02 08:20:18 +0200

    refactor: extract window  creation to separate module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate piston_window;
 extern crate find_folder;
 
-use piston_window::*;
 
 mod types;
 mod draw;
@@ -10,49 +9,13 @@ mod food;
 mod snake;
 mod ui;
 mod game;
+mod window;
 
 
 fn main() {
     
     let viewport: (u32, u32) = (80, 58);
     let mut game = game::Game::new(viewport);
-    create_window(viewport, &mut game);
-}
-
-fn create_window(viewport: (u32, u32), game: &mut game::Game) {
-    const UPDATES_PER_SECOND: u64 = 60;
-
-    let window_resolution: (u32, u32) = (viewport.0 * 10, (viewport.1 * 10) + 20);
-    
-    let mut window: PistonWindow = WindowSettings::new("Snake", window_resolution)
-        .exit_on_esc(true)
-        .vsync(true)
-        .resizable(false)
-        .build()
-        .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
-
-    let assets = find_folder::Search::ParentsThenKids(3, 3).for_folder("assets").unwrap();
-
-    let ref font = assets.join("FiraSans-Regular.ttf");
-    let factory = window.factory.clone();
-    let mut glyphs = Glyphs::new(font, factory, TextureSettings::new()).unwrap();
-    
-
-    let mut events = Events::new(EventSettings::new())
-        .max_fps(UPDATES_PER_SECOND)
-        .ups(UPDATES_PER_SECOND);
-
-    while let Some(e) = events.next(&mut window) {
-        if let Some(Button::Keyboard(key)) = e.press_args() {
-            game.key_pressed(key);
-        }
-        window.draw_2d(&e, |_c, g| {
-            clear(theme::BACKGROUND_COLOR, g);
-            game.draw(&_c, g, &mut glyphs); 
-        });
-
-        e.update(|arg| {
-            game.update(arg.dt);
-        });
-    }
+    let mut game_window = window::GameWindow::new(viewport);
+    game_window.game_loop(&mut game);
 }
---
+++ src/window.rs
@@ -0,0 +1,62 @@
+use piston_window::*;
+
+use crate::game::{Game};
+use crate::theme;
+
+pub struct GameWindow {
+    window: PistonWindow,
+    events: Events,
+    font: Glyphs
+}
+
+impl GameWindow {
+    pub fn new(viewport: (u32, u32)) -> GameWindow {
+        const UPDATES_PER_SECOND: u64 = 60;
+
+        let window_resolution: (u32, u32) = (viewport.0 * 10, (viewport.1 * 10) + 20);
+        
+        let window: PistonWindow = WindowSettings::new("Snake", window_resolution)
+            .exit_on_esc(true)
+            .vsync(true)
+            .resizable(false)
+            .build()
+            .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+
+        let assets = find_folder::Search::ParentsThenKids(3, 3).for_folder("assets").unwrap();
+
+        let ref font = assets.join("FiraSans-Regular.ttf");
+        let factory = window.factory.clone();
+        let font = Glyphs::new(font, factory, TextureSettings::new()).unwrap();
+        
+
+        let events = Events::new(EventSettings::new())
+            .max_fps(UPDATES_PER_SECOND)
+            .ups(UPDATES_PER_SECOND);
+        
+        GameWindow{
+            window,
+            events,
+            font
+        }
+    }
+
+    pub fn game_loop(&mut self, game: &mut Game) {
+        let events = &mut self.events;
+        let window = &mut self.window;
+        let font = &mut self.font;
+
+        while let Some(e) = events.next(window) {
+            if let Some(Button::Keyboard(key)) = e.press_args() {
+                game.key_pressed(key);
+            }
+            window.draw_2d(&e, |_c, g| {
+                clear(theme::BACKGROUND_COLOR, g);
+                game.draw(&_c, g, font); 
+            });
+
+            e.update(|arg| {
+                game.update(arg.dt);
+            });
+        }
+    }
+}
